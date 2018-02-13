edcdd559e631ed3de3b4e7ac6ad12ddf1dc6cca9
GaborMartin <gabormartin94@gmail.com>
2018-02-13 15:36:35 +0100

    Implemented Plaza interface in class PlazaImpl

---
+++ src/main/java/com/codecool/plaza/api/PlazaImpl.java
@@ -0,0 +1,55 @@
+package com.codecool.plaza.api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlazaImpl implements Plaza {
+    private List<Shop> shops;
+    private boolean isOpen;
+
+    public PlazaImpl(boolean isOpen) {
+        this.shops = new ArrayList<Shop>();
+        this.isOpen = false;
+    }
+
+    @Override
+    public List<Shop> getShops() throws PlazaIsClosedException {
+        return shops;
+    }
+
+    @Override
+    public void addShop(Shop shop) throws ShopAlreadyExistsException, PlazaIsClosedException {
+        shops.add(shop);
+    }
+
+    @Override
+    public void removeShop(Shop shop) throws NoSuchShopException, PlazaIsClosedException {
+        shops.remove(shop);
+    }
+
+    @Override
+    public Shop findShopByName(String name) throws NoSuchShopException, PlazaIsClosedException {
+        for (int i = 0; i < shops.size(); i++) {
+            if (shops.get(i).getName().equals(name)) {
+                return shops.get(i);
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean isOpen() {
+        return isOpen;
+    }
+
+    @Override
+    public void open() {
+        isOpen = true;
+    }
+
+    @Override
+    public void close() {
+        isOpen = false;
+    }
+}
+
