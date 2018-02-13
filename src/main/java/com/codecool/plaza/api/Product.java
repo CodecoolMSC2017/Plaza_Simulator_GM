96fbded6671e160a3f2b3e6f9fed8f5d497db986
GaborMartin <gabormartin94@gmail.com>
2018-02-13 16:39:35 +0100

    Implemented class Product

---
+++ src/main/java/com/codecool/plaza/api/Product.java
@@ -0,0 +1,34 @@
+package com.codecool.plaza.api;
+
+public abstract class Product {
+    protected String name;
+    protected long barcode;
+    protected String manufacturer;
+
+    protected Product(String name, long barcode, String manufacturer) {
+        this.name = name;
+        this.barcode = barcode;
+        this.manufacturer = manufacturer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getBarcode() {
+        return barcode;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+            "name='" + name + '\'' +
+            ", barcode=" + barcode +
+            ", manufacturer='" + manufacturer + '\'' +
+            '}';
+    }
+}
