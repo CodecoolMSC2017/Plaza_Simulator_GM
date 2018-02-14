7ea91f095393ca4550b04b72b32818d6d66adca6
GaborMartin <gabormartin94@gmail.com>
2018-02-14 11:58:07 +0100

    Exception handling added to methods

--- src/main/java/com/codecool/plaza/api/PlazaImpl.java
+++ src/main/java/com/codecool/plaza/api/PlazaImpl.java
@@ -16,27 +16,45 @@ public class PlazaImpl implements Plaza {
 
     @Override
     public List<Shop> getShops() throws PlazaIsClosedException {
-        return shops;
+        if(this.isOpen() == true) {
+            return shops;
+        }
+        throw new PlazaIsClosedException("Plaza is closed, sorry!");
     }
 
     @Override
     public void addShop(Shop shop) throws ShopAlreadyExistsException, PlazaIsClosedException {
+        if(this.isOpen() == false) {
+            throw new PlazaIsClosedException("Plaza is closed, sorry!");
+        }
+        if(shops.contains(shop)) {
+            throw new ShopAlreadyExistsException("This shop already exists!");
+        }
         shops.add(shop);
     }
 
     @Override
     public void removeShop(Shop shop) throws NoSuchShopException, PlazaIsClosedException {
+        if(this.isOpen() == false) {
+            throw new PlazaIsClosedException("Plaza is closed, sorry!");
+        }
+        if(!shops.contains(shop)) {
+            throw new NoSuchShopException("There's no such shop!");
+        }
         shops.remove(shop);
     }
 
     @Override
     public Shop findShopByName(String name) throws NoSuchShopException, PlazaIsClosedException {
+        if(this.isOpen() == false) {
+            throw new PlazaIsClosedException("Plaza is closed, sorry!");
+        }
         for (int i = 0; i < shops.size(); i++) {
             if (shops.get(i).getName().equals(name)) {
                 return shops.get(i);
             }
         }
-        return null;
+        throw new NoSuchShopException("There's no such shop");
     }
 
     @Override
